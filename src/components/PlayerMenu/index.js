720dab52f59689f2ff25a441801bbd1dbdb54880
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2023-10-10 15:22:37 -0600

    Update index.js

--- src/components/PlayerMenu/index.js
+++ src/components/PlayerMenu/index.js
@@ -76,16 +76,24 @@ export default class PlayerMenu extends Component {
           onClose={this.handleClose}
         >
           <MenuItem
-  onClick={async () => {
+  onClick={() => {
     try {
       // Construir la URL de redirección
       const redirectUrl = `${server}/api/v1/redirectdownload/${encodeURIComponent(metadata.name)}?a=${auth}&id=${id}`;
 
-      // Crear un Intent
-      const intentUri = `intent://#Intent;action=android.intent.action.VIEW;scheme=http;type=${metadata.mimeType};end`;
+      // Crear un Intent con la acción VIEW
+      const intent = new Intent("android.intent.action.VIEW");
 
-      // Intentar abrir la URL en un navegador web y permitir al usuario seleccionar la aplicación
-      window.location.href = intentUri;
+      // Establecer la URL en el Intent
+      intent.setData(Uri.parse(redirectUrl));
+
+      // Solicitar al sistema que abra el selector de aplicaciones
+      intent.addCategory("android.intent.category.BROWSABLE");
+      intent.setComponent(null);
+      intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+      // Iniciar el Intent
+      window.Android.startActivity(intent);
     } catch (error) {
       console.error('Error al abrir la URL:', error);
     }
@@ -93,6 +101,7 @@ export default class PlayerMenu extends Component {
 >
   Reproductor
 </MenuItem>
+
 
           <Divider />
           <MenuItem
