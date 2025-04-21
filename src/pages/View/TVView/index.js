bc17286b78a13a3f6b227de1a9c19b002f863c08
Arturo Romero <116369556+RArtutos@users.noreply.github.com>
2025-04-21 10:10:51 -0600

    Update index.js

--- src/pages/View/TVView/index.js
+++ src/pages/View/TVView/index.js
@@ -64,13 +64,15 @@ export class TVBView extends Component {
 
   componentDidMount() {
     let { metadata, ui_config } = this.state;
-    let year = this.state.metadata.releaseDate.split("-")[0];
+    let year = metadata.releaseDate
+      ? new Date(metadata.releaseDate).getFullYear()
+      : new Date().getFullYear();
 
     seo({
       title: `${ui_config.title || "RepelisPlus"} - ${
           metadata.title || metadata.name
       } (${year}) - Español Latino Gratis`,
-      description: `Watch ${metadata.title || metadata.name} on ${
+      description: `Ver ${metadata.title || metadata.name} en ${
           ui_config.title || "RepelisPlus"
       }! — ${metadata.overview}`,
       image: metadata.backdropPath,
